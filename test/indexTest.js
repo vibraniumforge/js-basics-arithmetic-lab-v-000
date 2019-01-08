f5dd2884a0016eefe7938b544c953cfdfc54c2f1
Greg Morgan <gregm9999@gmail.com>
2019-01-08 04:59:06 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,7 +2,7 @@ const expect = chai.expect;
 spyOnNumberIsInteger.restore();
 let newID = oldID + 1000000000;
 // let ageIsValid = Number.isInteger(currentAge);
-// let randomNumber = Math.floor(Math.random() * 20) + 1;
+let randomNumber = Math.floor(Math.random() * 20) + 1;
 let randomInteger = Math.floor(Math.random() * 10) + 1;
 let randomUserID = Math.floor(Math.random() * 10) + 1000000000;
 
